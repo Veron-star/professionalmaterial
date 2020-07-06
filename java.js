e5a9b33e30d14635c9fa4270b61a7ce5ec53d4e1
veronica <veron_yan@yahoo.com.sg>
2020-07-06 21:40:45 +0800

    styling

--- java.js
+++ java.js
@@ -1,3 +1,8 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header.container');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
@@ -6,8 +11,15 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
     if(scroll_position > 250) {
-        Headers.style.backgroundColor = '#29323c';
+        header.style.backgroundColor = '#29323c';
     } else {
         header.style.backgroundColor = 'transparent';
     }
+});
+
+menu_item.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
 });
